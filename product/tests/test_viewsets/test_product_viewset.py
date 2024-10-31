871f713679734009c7d1f8cc8582ca53a475f470
piegez <jessicamsampaio@gmail.com>
2024-10-31 19:55:13 -0300

    feat: adicao autenticacao

--- product/tests/test_viewsets/test_product_viewset.py
+++ product/tests/test_viewsets/test_product_viewset.py
@@ -2,6 +2,7 @@ import json
 
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
+from rest_framework.authtoken.models import Token
 
 from django.urls import reverse
 
@@ -14,6 +15,9 @@ class TestProductViewSet(APITestCase):
 
     def setUp(self):
         self.user = UserFactory()
+        token = Token.objects.create(user=self.user)
+        token.save()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
 
         self.product = ProductFactory(
             title='pro controller',
@@ -28,9 +32,9 @@ class TestProductViewSet(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         product_data = json.loads(response.content)
 
-        self.assertEqual(product_data[0]['title'], self.product.title)
-        self.assertEqual(product_data[0]['price'], self.product.price)
-        self.assertEqual(product_data[0]['active'], self.product.active)
+        self.assertEqual(product_data['results'][0]['title'], self.product.title)
+        self.assertEqual(product_data['results'][0]['price'], self.product.price)
+        self.assertEqual(product_data['results'][0]['active'], self.product.active)
 
     def test_create_product(self):
         category = CategoryFactory()
@@ -45,7 +49,7 @@ class TestProductViewSet(APITestCase):
             data=json.dumps(data),
             content_type='application/json'
         )
-        print(response.data)
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         created_product = Product.objects.get(title='notebook')
